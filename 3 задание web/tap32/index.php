4d111f61fc563ee05e6378d0c171e5dc8c8585c0
Danat122 <85071124+Danat122@users.noreply.github.com>
2021-05-30 21:55:10 +0600

    Add files via upload

---
+++ 3 задание web/tap32/index.php
@@ -0,0 +1,61 @@
+<?php
+
+
+$month = 12;
+
+switch ($month){
+    case 1:
+        echo "Qys";
+        break;
+
+        case 2:
+            echo "Qys";
+            break;
+
+
+            case 3:
+                echo "Koktem";
+                break;
+        
+                case 4:
+                    echo "Koktem";
+                    break;
+
+                    case 5:
+                        echo "Koktem";
+                        break;
+                
+                        case 6:
+                            echo "Jaz";
+                            break;
+
+                            case 7:
+                                echo "Jaz";
+                                break;
+                        
+                                case 8:
+                                    echo "Jaz";
+                                    break;
+
+                                    case 9:
+                                        echo "Kuz";
+                                        break;
+
+                                        case 10:
+                                            echo "Kuz";
+                                            break;
+
+                                            case 11:
+                                                echo "Kuz";
+                                                break;
+
+                                                case 12:
+                                                    echo "Qys";
+                                                    break;
+
+
+
+                                    
+}
+
+?>
